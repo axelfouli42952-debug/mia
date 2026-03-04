960bbbb36eb89d1d4961ab5229af0c8c226e053a
axelfouli42952-debug <axelfouli42952@gmail.com>
2026-03-04 07:47:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+print("TOKEN FROM ENV:", BOT_TOKEN)
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message or not update.message.text:
@@ -12,4 +13,5 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
     app.run_polling()
