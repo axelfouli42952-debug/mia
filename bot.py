c33da2a930b26c673a03130522ec89113121f77e
axelfouli42952-debug <axelfouli42952@gmail.com>
2026-03-04 07:35:40 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message or not update.message.text:
+        return
+    await update.message.reply_text("Я жива 😌")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.run_polling()
